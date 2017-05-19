d62298ac8b420f003460a0e19bc9d35c021a7d3d
Nicolas Galler <ngaller@gmail.com>
2017-05-19 11:16:46 -0400

    Clean up date fields on streaming event records

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicocrm:syncforce',
-  version: '0.0.8',
+  version: '0.0.9',
   // Brief, one-line summary of the package.
   summary: 'Automate synchronization between Mongo and Salesforce Entities',
   // URL to the Git repository containing the source code for this package.
