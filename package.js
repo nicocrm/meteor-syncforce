649e01c838b5cc899a6d5b8952caa0f4940b9157
Nicolas Galler <ngaller@gmail.com>
2017-06-05 07:22:02 -0400

    Fix subscription to use 'created' event type instead of 'inserted'

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicocrm:syncforce',
-  version: '0.0.10',
+  version: '0.0.11',
   // Brief, one-line summary of the package.
   summary: 'Automate synchronization between Mongo and Salesforce Entities',
   // URL to the Git repository containing the source code for this package.
