220f9716590873bddc5a72917c6987f127799db4
Nicolas Galler <ngaller@gmail.com>
2017-06-12 16:07:29 -0400

    add some concurrency checks in lookups

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicocrm:syncforce',
-  version: '0.0.13',
+  version: '0.0.14',
   // Brief, one-line summary of the package.
   summary: 'Automate synchronization between Mongo and Salesforce Entities',
   // URL to the Git repository containing the source code for this package.
