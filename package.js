0a1013a472ed5ea7eb00a36f0fed27a0d8286759
Nicolas Galler <ngaller@gmail.com>
2017-06-14 21:43:28 -0400

    Debugging output

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicocrm:syncforce',
-  version: '0.0.15',
+  version: '0.0.16',
   // Brief, one-line summary of the package.
   summary: 'Automate synchronization between Mongo and Salesforce Entities',
   // URL to the Git repository containing the source code for this package.
